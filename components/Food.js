aa27ba282e8fe1309f8c2f84b9f3b01c6d9e1290
Bugaev Andrey <dihlo1@yandex.ru>
2018-10-29 16:00:32 +0900

    SAD-21

--- components/Food.js
+++ components/Food.js
@@ -1,15 +1,79 @@
-import React from 'react'
-import { TouchableOpacity, Text } from 'react-native'
-import { Actions } from 'react-native-router-flux'
+import React, { Component } from 'react';
+import { Text, View, ScrollView } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { List, Badge, WhiteSpace, WingBlank, Pagination, Flex, Button, Card } from 'antd-mobile-rn';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const Food = () => {
-   const goToHome = () => {
-      Actions.home()
-   }
-   return (
-      <TouchableOpacity style = {{ margin: 128 }} onPress = {goToHome}>
-         <Text>This is Food</Text>
-      </TouchableOpacity>
-   )
+export default class Food extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {	
+		}
+	}
+
+
+
+	render () {
+
+    const locale = {
+      prevText: '<',
+      nextText: '>',
+    };
+
+	return (
+	  <ScrollView>
+	  	<WhiteSpace />   
+	  	<WingBlank size="sm">
+	  		<Text style={{color: 'black', fontSize: 14}}>29.10.2018</Text>
+	  	</WingBlank>
+	    <WhiteSpace />       
+	    <WingBlank size="sm">
+		    <Card full>
+		      <Card.Header
+		        title="Завтрак"
+		        thumb={<Icon name="ios-restaurant" size={25} color="#FF9933" style={{paddingRight: 10 }}/>}		        
+		      />
+		      <Card.Body>
+		        <Text style={{fontSize: 18, paddingLeft: 15, color: 'black'}}>Каша манная 150 г.</Text>
+		        <Text style={{fontSize: 18, paddingLeft: 15, color: 'black'}}>Компот малиновый 250 г.</Text>
+		        <Text style={{fontSize: 18, paddingLeft: 15, color: 'black'}}>Хлеб 20 г.</Text>
+		      </Card.Body>
+		      <Card.Footer content="Колорийность" extra={<Text>500 ккал</Text>} />
+		    </Card>
+	    </WingBlank>
+	    <WhiteSpace size="sm" />
+	    <WingBlank size="sm">
+		    <Card full>
+		      <Card.Header
+		        title="Полдник"
+		        thumb={<Icon name="ios-beer" size={25} color="#FF9933" style={{paddingRight: 10 }}/>}		        
+		      />
+		      <Card.Body>
+		        <Text style={{fontSize: 18, paddingLeft: 15, color: 'black'}}>Яблоко 150 г.</Text>
+		        <Text style={{fontSize: 18, paddingLeft: 15, color: 'black'}}>Булочка 250 г.</Text>
+		      </Card.Body>
+		      <Card.Footer content="Колорийность" extra={<Text>500 ккал</Text>} />
+		    </Card>
+	    </WingBlank>
+	    <WhiteSpace size="sm" />
+	   	<WingBlank size="sm">
+		    <Card full>
+		      <Card.Header
+		        title="Обед"
+		        thumb={<Icon name="md-nutrition" size={25} color="#FF9933" style={{paddingRight: 10 }}/>}		        
+		      />
+		      <Card.Body>
+		        <Text style={{fontSize: 18, paddingLeft: 15, color: 'black'}}>Суп борщ 150 г.</Text>
+		        <Text style={{fontSize: 18, paddingLeft: 15, color: 'black'}}>Котлета 250 г.</Text>
+		        <Text style={{fontSize: 18, paddingLeft: 15, color: 'black'}}>Хлеб 20 г.</Text>
+		      </Card.Body>
+		      <Card.Footer content="Колорийность" extra={<Text>500 ккал</Text>} />
+		    </Card>
+	    </WingBlank>
+	    <WhiteSpace size="sm" />
+	    <Pagination total={5} current={1} locale={locale} />
+	    <WhiteSpace />  
+	  </ScrollView>
+	)}
 }
-export default Food
